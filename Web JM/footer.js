cd5c2645bf9eb2dea93bd190c024c080a3cd789c
christiangoni <chistiansen@gmail.com>
2016-09-22 16:17:02 +0200

    footer: 4xcol-3 replace by 3xcol-4

---
+++ Web JM/footer.js
@@ -0,0 +1,42 @@
+<script type=\"text/javascript\">
+//<![CDATA[
+document.write('esto seria el footer')
+/*
+	<footer>
+	<div class=\"container\">
+		<div class=\"row\">
+			<div class=\"col-lg-4\">
+				<div class=\"widget\">
+					<h5 class=\"widgetheading\">Contacto</h5>
+					<address>
+					Despacho Jurídico para la atención personal al Cliente<br>Avda/Pío XII - nº 5 piso 1º Oficina nº 1<br>
+					</address>
+					<p>
+						<i class=\"fa fa-phone\" aria-hidden=\"true\"></i> (+34) 626225434 <br>
+						<span class=\"glyphicon glyphicon-envelope\"> Jmescala@micap.es
+					</p>
+				</div>
+			</div>
+			<div class=\"col-lg-4\">
+				<div class=\"widget\">
+					<h5 class=\"widgetheading\">Páginas relacionadas</h5>
+					<ul class=\"link-list\">
+						<li><a href=\"http://www.micap.es/\">MICAP</a></li>
+						<li><a href=\"http://www.abogacia.es/\">Consejo General Abogacia</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class=\"col-lg-4\">
+				<div class=\"widget\">
+					<h5 class=\"widgetheading\">Últimos articulos</h5>
+					<ul class=\"link-list\">
+						<li><a href=\"http://elpais.com/tag/derecho_civil/a/\">Ultimas noticias derecho civil</a></li>
+						<li><a href=\"http://www.ambitojuridico.com/mercantil.asp\">Ultimas noticias derecho mercantil</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>')*/
+//]]>
+</script> 
